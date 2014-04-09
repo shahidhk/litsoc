8e96a433d2398d203ddb0b3c45827018afc9a256
Shahidh <muhammedshahid.k@gmail.com>
2014-04-10 00:56:39 +0530

    Added admins

---
+++ clubs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from models import Club, MusicRoomTeam, MusicRoomSlot
+
+admin.site.register(Club)
+admin.site.register(MusicRoomTeam)
+admin.site.register(MusicRoomSlot)
---
+++ events/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from models import Venue, Event
+
+admin.site.register(Venue)
+admin.site.register(Event)
